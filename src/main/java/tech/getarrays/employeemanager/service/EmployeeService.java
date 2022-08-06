09fe643e0c926a8bf51b8959054f7fccd590b677
Gabriel Soares <gabrielalcantarars@gmail.com>
2022-08-06 01:56:02 -0300

    creating main http methods

---
+++ src/main/java/tech/getarrays/employeemanager/service/EmployeeService.java
@@ -0,0 +1,42 @@
+package tech.getarrays.employeemanager.service;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import tech.getarrays.employeemanager.exception.UserNotFoundException;
+import tech.getarrays.employeemanager.model.Employee;
+import tech.getarrays.employeemanager.repo.EmployeeRepo;
+
+@Component
+public class EmployeeService {
+	private final EmployeeRepo employeeRepo;
+	
+	@Autowired
+	public EmployeeService(EmployeeRepo employeeRepo) {
+		this.employeeRepo = employeeRepo;
+	}
+	
+	public Employee addEmployee(Employee employee) {
+		employee.setEmployeeCode(UUID.randomUUID().toString());
+		return employeeRepo.save(employee);
+	}
+	
+	public java.util.List<Employee> findAllEmployees() {
+		return employeeRepo.findAll();
+	}
+	
+	public Employee updateEmployee(Employee employee) {
+		return employeeRepo.save(employee);
+	}
+	
+	public Employee findEmployeeById(Long id) {
+		return employeeRepo.findEmployeeById(id)
+				.orElseThrow(() -> new UserNotFoundException("User by id " + id + " was not found"));
+	}
+	
+	public void deleteEmployee(Long id) {
+		employeeRepo.deleteEmployeeById(id);
+	}
+}
